67bdb09552be137caee82f7085d865db300baf2f
hyperbotauthor <hyperchessbotauthor@gmail.com>
2021-10-05 13:26:15 +0000

    Update main

---
+++ trymodule/demo.js
@@ -0,0 +1,9 @@
+const { Pos } = require("@publishvue/chessops")
+
+const START_FEN_STANDARD = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+
+const pos = Pos()
+
+pos.setFen("rn2kbnr/p1Pppppp/b7/8/8/8/PPP1PPPP/RNBQKBNR w KQkq - 1 5")
+
+console.log(pos.allLegalSans())
