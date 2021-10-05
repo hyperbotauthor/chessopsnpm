abb96841b8843047dc8bf6f7fe115a5fef678a6b
hyperbotauthor <hyperchessbotauthor@gmail.com>
2021-10-05 10:28:52 +0000

    Test raw legal ucis

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,9 +1,25 @@
-const { Pos } = require("../dist/index.js")
+const { Pos } = require("../dist/index.js");
+
+const { arrayEquals } = require("../utils.js");
 
 const START_FEN_STANDARD = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
 const pos = Pos()
 
+const startRawLegalUcis = [
+    'b1a3', 'b1c3', 'g1f3',
+    'g1h3', 'a2a3', 'a2a4',
+    'b2b3', 'b2b4', 'c2c3',
+    'c2c4', 'd2d3', 'd2d4',
+    'e2e3', 'e2e4', 'f2f3',
+    'f2f4', 'g2g3', 'g2g4',
+    'h2h3', 'h2h4'
+]
+
 test('standard starting fen should be ' + START_FEN_STANDARD, () => {
     expect(pos.reportFen()).toBe(START_FEN_STANDARD);
+});
+
+test('start raw legal ucis should be ' + startRawLegalUcis, () => {
+    expect(arrayEquals(pos.rawLegalUcis(), startRawLegalUcis)).toBe(true);
 });
